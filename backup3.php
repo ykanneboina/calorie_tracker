8106ae373993f1d3a7189d975f8ac65e40e1f7a6
Yoga Kanneboina <yogapriyak7@gmail.com>
2018-05-21 00:30:00 +0000

    My third commit

---
+++ backup3.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href= "style.css"/>
+        <title>DHS Calorie Tracker</title>
+        <link rel = "shortcut icon" type= "image/jpg" href = "dhs.jpg">
+    </head>
+    <body>
+        <div class = "fixed">
+            <div class= "header">
+                <div class="pull-right">
+                    <ul class="nav nav-pills">
+                    <li>
+                        <a class= "white" href="index.html">All</a>
+                    </li>
+                    <li>
+                        <a class= "white" href="breakfast.php">Breakfast</a>
+                    </li>
+                    <li>
+                        <a class= "white" href="lunch.php">Lunch</a>
+                    </li>
+                    <li>
+                        <a class= "white" href="snacks.php">Snacks</a>
+                    </li>
+                    <li>
+                        <a class= "white" href="drinks.php">Drinks</a>
+                    </li>
+                    <li>
+                        <a class= "white" href="about.php">About</a>
+                    </li>
+                    </ul>
+                </div>  
+                <div class="container" ><h1>DHS Calorie Tracker About</h1></div>
+            </div>
+            <img class= "line"  src="color-line.jpg">
+        </div> 
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class = "container ">
+         <img class = "container col-sm-4"  src="sy.jpg"> <p class="aboutme">Hello! Welcome to the Dublin High School Calorie Tracker. This calorie tracker is unique to the DHS lunch, breakfast, snack menu.
+         This way, students can be more aware about how much calories they are eating and how healthy the food that they they buy is.
+         One of the key features of this website is that it gives the recommended amount of calorie intake for a person depending on their age,
+         gender, and activity level. Another feature is that it adds up the calories that you ate in total and gives the percentage of the 
+         recommended calories that you ate. It also gives the total amount of calories that you ate. Our goal is to make this website accessible 
+         to everyone who eats food from Dublin High School so that we can become a more healthier and aware community. 
+        </p>
+    <br>
+    <br>
+    <img class = "container" src="food.jpg">
+        </div>
+    </body>
+</html>
+
+<html>
+<body>
+
